2e28054ef8a5cff6cbc0a57500cdb4fe9d9fd7ea
Rafael Klock <kklockk@gmail.com>
2019-08-04 15:49:54 -0300

    updates

--- tts.py
+++ tts.py
@@ -1,10 +1,12 @@
+# This app makes a call between number A and number B.
+
 from totalvoice.cliente import Cliente
 import requests
 
 api_totalvoice = Cliente("089f722451983a4ef92db0e5eba812b0", 'api.totalvoice.com.br')
 
-src_number = '48996516004'
-dst_number = '48996516005'
+src_number = input('Insert A number: ')
+dst_number = input('Insert B number: ')
 
 response = api_totalvoice.chamada.enviar(src_number, dst_number, gravar_audio=True)
 print(response)
